b10e3819fdd8617303c329c2a1f547d40c4610b4
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 12:58:37 +0100

    feat: add route for authentication

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { authenticationUserController } from "../use-cases/users/authentication";
 import { createUserController } from "../use-cases/users/create-user";
 import { deleteUserController } from "../use-cases/users/delete-user";
 import { getUsersController } from "../use-cases/users/get-users";
@@ -11,6 +12,10 @@ usersRouter.post("/", (request, response) =>
 
 usersRouter.get("/", (request, response) =>
   getUsersController.handle(request, response)
+);
+
+usersRouter.get("/authenticate", (request, response) =>
+  authenticationUserController.handle(request, response)
 );
 
 usersRouter.delete("/:id", (request, response) =>
