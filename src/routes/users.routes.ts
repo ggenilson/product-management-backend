54ce5323a9cb68bb143c9a5035ee3554f89d9945
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 11:43:59 +0100

    feat: add delete-user route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createUserController } from "../use-cases/users/create-user";
+import { deleteUserController } from "../use-cases/users/delete-user";
 import { getUsersController } from "../use-cases/users/get-users";
 
 const usersRouter = Router();
@@ -10,6 +11,10 @@ usersRouter.post("/", (request, response) =>
 
 usersRouter.get("/", (request, response) =>
   getUsersController.handle(request, response)
+);
+
+usersRouter.delete("/:id", (request, response) =>
+  deleteUserController.handle(request, response)
 );
 
 export default usersRouter;
