b246cef8dd6714e56200dd43c45236d7b1370dd1
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 14:24:22 +0100

    feat: add privates routes

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { authenticateMiddleware } from "../middlewares/auth";
 import { createProductController } from "../use-cases/products/create-product";
 import { deleteProductController } from "../use-cases/products/delete-product";
 import { getProductsController } from "../use-cases/products/get-products";
 
 const productsRouter = Router();
+
+productsRouter.use(authenticateMiddleware.auth);
 
 productsRouter.post("/", (request, response) =>
   createProductController.handle(request, response)
