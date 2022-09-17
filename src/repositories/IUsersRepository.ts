7c5292bcfb058080e84a8fe58138c03d23fff71d
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 11:05:11 +0100

    feat: add file repository

--- src/repositories/IUsersRepository.ts
+++ src/repositories/IUsersRepository.ts
@@ -3,4 +3,6 @@ import { User } from "../entities/User";
 export interface IUsersRepository {
   findByEmail: (email: string) => Promise<User>;
   save: (user: User) => Promise<User>;
+  updateUserFile: () => void;
+  getUserFile: () => Promise<User[]>;
 }
--- src/repositories/implementations/FileUsersRepository.ts
+++ src/repositories/implementations/FileUsersRepository.ts
@@ -1,8 +1,31 @@
+import fs from "fs";
 import { User } from "../../entities/User";
 import { IUsersRepository } from "../IUsersRepository";
 
 export class FileUsersRepository implements IUsersRepository {
   private users: User[] = [];
+
+  updateUserFile(): void {
+    fs.writeFile("users.json", JSON.stringify(this.users), (err) => {
+      if (err) {
+        console.log("something went wrong");
+      } else {
+        console.log("user updated");
+      }
+    });
+  }
+
+  async getUserFile(): Promise<User[]> {
+    try {
+      const data = await fs.promises.readFile("users.json", "utf8");
+
+      const dataParsed = JSON.parse(data);
+
+      return dataParsed;
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
 
   async findByEmail(email: string): Promise<User> {
     const user = this.users.find((user) => user.email === email);
@@ -12,6 +35,8 @@ export class FileUsersRepository implements IUsersRepository {
 
   async save(user: User): Promise<User> {
     this.users.push(user);
+
+    this.updateUserFile();
 
     return user;
   }
