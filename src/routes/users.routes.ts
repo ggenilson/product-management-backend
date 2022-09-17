71f6b289092597448f81c4c3dc05314ae31c2602
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 11:29:06 +0100

    feat: add route for get users

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { createUserController } from "../use-cases/users/create-user";
+import { getUsersController } from "../use-cases/users/get-users";
 
 const usersRouter = Router();
 
 usersRouter.post("/", (request, response) =>
   createUserController.handle(request, response)
+);
+
+usersRouter.get("/", (request, response) =>
+  getUsersController.handle(request, response)
 );
 
 export default usersRouter;
