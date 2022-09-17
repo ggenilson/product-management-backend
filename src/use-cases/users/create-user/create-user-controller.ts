1a59b6af80a7fc73b908d117f6a81d5022053293
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 10:18:35 +0100

    feat: create create user controller

---
+++ src/use-cases/users/create-user/create-user-controller.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { CreateUserUseCase } from "./create-user-use-case";
+
+export class CreateUserController {
+  constructor(private createUserUseCase: CreateUserUseCase) {}
+
+  async handle(request: Request, response: Response): Promise<Response> {
+    const { name, username, email, password } = request.body;
+
+    try {
+      const user = await this.createUserUseCase.execute({
+        name,
+        username,
+        email,
+        password,
+      });
+
+      return response.status(201).send(user);
+    } catch (err) {
+      return response.status(400).json({
+        message: err.message || "unexpected error",
+      });
+    }
+  }
+}
