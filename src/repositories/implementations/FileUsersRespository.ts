afdbc4378203e6241c9b0eb7b6002446da0dc8c1
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 10:24:55 +0100

    feat: create repository for users

---
+++ src/repositories/implementations/FileUsersRespository.ts
@@ -0,0 +1,18 @@
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class FileUsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  async findByEmail(email: string): Promise<User> {
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
+  }
+
+  async save(user: User): Promise<User> {
+    this.users.push(user);
+
+    return user;
+  }
+}
