1cabe1a001ac8ceb6bb0477102aea1b770b986a2
ggenilson <genilsonaraujo20002002@gmail.com>
2022-09-17 12:48:18 +0100

    feat: create authentication module

---
+++ src/config/auth.ts
@@ -0,0 +1,4 @@
+export = {
+  secret: "e3e34490db79ee9403803ab360d500c9",
+  expiresIn: "1d",
+};
---
+++ src/use-cases/users/authentication/authentication-user-use-case.ts
@@ -0,0 +1,34 @@
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import AuthConfig from "../../../config/auth";
+import { IUsersRepository } from "../../../repositories/IUsersRepository";
+import { IAuthenticationUserDTO } from "./authentication-user-dto";
+
+export class AuthenticationUserUseCase {
+  constructor(private userRepository: IUsersRepository) {}
+
+  async execute({ email, password }: IAuthenticationUserDTO) {
+    const user = await this.userRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("incorrect email or password");
+    }
+
+    const verifyPass = await bcrypt.compare(password, user.password);
+
+    if (!verifyPass) {
+      throw new Error("incorrect email or password");
+    }
+
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET ?? "", {
+      expiresIn: AuthConfig.expiresIn,
+    });
+
+    const { password: _, ...userLogin } = user;
+
+    return {
+      user: userLogin,
+      token: token,
+    };
+  }
+}
